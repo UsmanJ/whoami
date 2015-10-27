7de312150ba5b2e2ae30134c439d63491ba21cf5
Usman Jamil <usman.jamil@live.co.uk>
2015-10-27 15:46:55 +0000

    displays distance between two locations

--- www/js/GeoCtrl.js
+++ www/js/GeoCtrl.js
@@ -2,18 +2,21 @@ whoami.controller('GeoCtrl', function($ionicPlatform, $cordovaGeolocation) {
 
   var self = this;
 
-  self.coor = "";
+  self.distance = "";
+  self.ownLat = "";
+  self.ownLong = "";
 
   var posOptions = {timeout: 4000, enableHighAccuracy: true};
 
   self.where = function(){
    $cordovaGeolocation.getCurrentPosition(posOptions)
    .then(function(position){
-                var lat  = position.coords.latitude
-                var long = position.coords.longitude
+                var lat  = position.coords.latitude;
+                self.ownLat = lat;
+                var long = position.coords.longitude;
+                self.ownLong = long;
                 // self.coor.push(lat);
                 // self.coor.push(long);
-                self.coor = "(" + lat + ", " + long + ")";
                 console.log('lat', lat);
                 console.log('long', long);
                 console.log('coor', self.coor);
@@ -23,13 +26,13 @@ whoami.controller('GeoCtrl', function($ionicPlatform, $cordovaGeolocation) {
 
   };
 
-  self.getDistance = function(lat1,lon1,lat2,lon2) {
+  self.getDistance = function(lat2,lon2) {
    var R = 6371; // Radius of the earth in km
-   var dLat = deg2rad(lat2-lat1);  // deg2rad below
-   var dLon = deg2rad(lon2-lon1);
+   var dLat = deg2rad(lat2-self.ownLat);  // deg2rad below
+   var dLon = deg2rad(lon2-self.ownLong);
    var a =
      Math.sin(dLat/2) * Math.sin(dLat/2) +
-     Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+     Math.cos(deg2rad(self.ownLat)) * Math.cos(deg2rad(lat2)) *
      Math.sin(dLon/2) * Math.sin(dLon/2)
      ;
    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
