fc37afe8df2265d74237b0142949e2928e9e64f7
Alexander Person <alexander@Leonards-MacBook-Pro.local>
2015-10-28 13:05:04 +0000

    facebook login and receiving details

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 
-var whoami = angular.module('whoami', ['ionic', 'ngCordova', 'ngOpenFB']);
+var whoami = angular.module('whoami', ['ionic','ionic.service.core', 'ngCordova', 'ngOpenFB']);
 
 whoami.run(function($ionicPlatform, ngFB) {
 	ngFB.init({appId: '1665971677011459'});
