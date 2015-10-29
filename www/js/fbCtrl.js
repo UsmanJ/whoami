0a3bea393e7c6db141203a61ef731047d5bd8772
Usman Jamil <usman.jamil@live.co.uk>
2015-10-29 09:08:41 +0000

    removed 2nd post request

--- www/js/fbCtrl.js
+++ www/js/fbCtrl.js
@@ -48,19 +48,19 @@ whoami.controller('fbCtrl', ["$scope", "$ionicModal", "$timeout", "$http", "$cor
 	function displayData(access_token)
 	{
     $http.get("https://graph.facebook.com/v2.2/me", {params: {access_token: access_token, fields: "name,email", format: "json" }}).then(function(result) {
-        var full_name = result.data.name;
+        var name = result.data.name;
         var email = result.data.email;
         // var html = '<table id="table" data-role="table" data-mode="column" class="ui-responsive"><thead><tr><th>Field</th><th>Info</th></tr></thead><tbody>';
         // html = html + "<tr><td>" + "Name" + "</td><td>" + name + "</td></tr>";
 
         // html = html + "</tbody></table>";
-        console.log(full_name)
+        console.log(name)
         console.log(email)
-        $scope.loginData.full_name = full_name;
+        $scope.loginData.name = name;
         $scope.loginData.email = email;
         console.log($scope.loginData);
 
-				var data = {'name': full_name, 'email': email}
+				var data = {'name': name, 'email': email}
 					$http({ method: "POST", url: "https://makerswhoami.herokuapp.com/locations", data: data }).then(function(data, status) {
 						alert('success');
 				}, function(error){
