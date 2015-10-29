4ec90a49878875c1c66c4c6d2fd4e17b9948493a
Alex Lemons <alexlemons1@gmail.com>
2015-10-29 09:17:41 +0000

    alex thurs morn commit

--- www/js/GeoCtrl.js
+++ www/js/GeoCtrl.js
@@ -1,59 +1,68 @@
-whoami.controller('GeoCtrl', function($ionicPlatform, $cordovaGeolocation) {
+whoami.controller('GeoCtrl', function($ionicPlatform, $cordovaGeolocation, $interval) {
+
+
+  var mock_data = {"Nikesh":"51.517437199999996, -0.0734174", "alex":"28.6139, 77.2090", "dom":"-90.0000, 0.0000", "usman":"42.3482, -75.1890", "emily":"-17.6667, -149.4167"}
+
 
   var self = this;
 
-  self.coor = "";
+  self.user_distances = {}
+  self.close_proximity = {}
+
+  self.myLatitude = ""
+  self.myLongitude = ""
 
   var posOptions = {timeout: 4000, enableHighAccuracy: true};
 
-  self.where = function(){
-   $cordovaGeolocation.getCurrentPosition(posOptions)
-   .then(function(position){
-                var lat  = position.coords.latitude
-                var long = position.coords.longitude
-                // self.coor.push(lat);
-                // self.coor.push(long);
-                self.coor = "(" + lat + ", " + long + ")";
-                console.log('lat', lat);
-                console.log('long', long);
-                console.log('coor', self.coor);
-            }, function(error){
-                console.log('error:', error);
-            });
+  $interval(function(){
+    $cordovaGeolocation.getCurrentPosition(posOptions)
+    .then(function(position){
+      self.myLatitude  = position.coords.latitude
+      self.myLongitude = position.coords.longitude
+      console.log('latitude', self.myLatitude);
+      console.log('longitude', self.myLongitude);
 
+      }, function(error){
+        console.log('error:', error);
+      });
+    }, 5000); //interval time in milliseconds
+
+  $interval(function(){
+    angular.forEach(mock_data, function(value, key) {
+      var arr = value.split(", ");
+      console.log('coor array', key, arr);
+      var difference = self.getDistance(parseFloat(arr[0]), parseFloat(arr[1]))
+      console.log('distance to', key, difference);
+      var name = key;
+      self.user_distances[name]= difference;
+      console.log(self.user_distances);
+
+      if(difference < 0.5){
+        self.close_proximity[name] = true;
+      }
+      console.log(self.close_proximity);
+    })
+  }, 7500);
+
+
+
+  self.getDistance = function(lat2,lon2) {
+    var R = 6371; // Radius of the earth in km
+    var dLat = deg2rad(lat2-self.myLatitude);  // deg2rad below
+    var dLon = deg2rad(lon2-self.myLongitude);
+    var a =
+      Math.sin(dLat/2) * Math.sin(dLat/2) +
+      Math.cos(deg2rad(self.myLatitude)) * Math.cos(deg2rad(lat2)) *
+      Math.sin(dLon/2) * Math.sin(dLon/2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+    self.distance = R * c; // Distance in km
+    return self.distance;
   };
+
+  function deg2rad(deg) {
+    return deg * (Math.PI/180)
+  };
+
+
+
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-   // return {
-   //   getPosition: function() {
-   //     return $ionicPlatform.ready()
-   //       .then(function() {
-   //         return $cordovaGeolocation.getCurrentPosition(positionOptions);
-   //       })
-   //   }
-   // };
